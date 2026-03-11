f21176aaf0184878a10d46281626e0ac98945978
Kavya6805 <kavymodi6805@gmail.com>
2026-03-11 17:20:15 +0530

    add extra works and assessment tasks.

---
+++ extra/filecmds.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+mkdir /home/einfochips/Kavya/bash_scripting_practice/extra/project
+cd project/
+touch file1.txt file2.txt file3.txt
+cp file1.txt copyfile1.txt
+mv file2.txt renamedfile2.txt
+rm file3.txt
